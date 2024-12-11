f00bf6dd9c92b63ef98ad3d1929c58498def64e6
PRAGATHI POOJARY <149041812+Pragathi-github@users.noreply.github.com>
2024-12-11 17:17:47 +0000

    Done with the hero carousel

---
+++ interior-design/src/Pages/LoginPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DefaultLayout from "../Layouts/DefaultLayoutHOC";
+
+const LoginPage = () => {
+  return <div>LoginPage</div>;
+};
+
+export default DefaultLayout(LoginPage);
